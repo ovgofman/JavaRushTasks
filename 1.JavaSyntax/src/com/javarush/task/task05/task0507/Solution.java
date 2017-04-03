6780158c2844a4fee015544d6987f1db3b87be76
ovgofman <o.v.gofman@gmail.com>
2017-04-04 00:51:32 +0300

    commit v7

--- 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
@@ -11,14 +11,12 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader typeDigits = new BufferedReader(new InputStreamReader(System.in));
 
-        double summary = 0;
-        double next = 0;
-        double counter = 0;
-        for (;next != -1; next = Double.parseDouble(typeDigits.readLine())) {
+        double summary = 0, next = 0, counter = 0;
+        for (; next != -1; next = Double.parseDouble(typeDigits.readLine())) {
             counter++;
             summary += next;
         }
-        System.out.println(summary/--counter);
+        System.out.println(summary / --counter);
     }
 }
 
