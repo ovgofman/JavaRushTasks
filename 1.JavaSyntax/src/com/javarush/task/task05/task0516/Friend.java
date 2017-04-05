c541accd972ce731be9c34e2f281e511b16f784d
ovgofman <o.v.gofman@gmail.com>
2017-04-06 00:07:18 +0300

    commit v9

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0516/Friend.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task05.task0516;
+
+/* 
+Друзей не купишь
+*/
+
+public class Friend {
+    String name;
+    int age;
+    char sex;
+
+    public void initialize(String name) {
+        this.initialize(name);
+    }
+
+    public void initialize(String name, int age) {
+        this.initialize(name, age);
+    }
+
+    public void initialize(String name, int age, char sex) {
+        this.initialize(name, age, sex);
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
