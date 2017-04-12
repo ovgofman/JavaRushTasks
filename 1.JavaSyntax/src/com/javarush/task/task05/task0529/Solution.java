5cbdbe93660ececfed31640eb179cd8f70703f5e
ovgofman <o.v.gofman@gmail.com>
2017-04-13 00:44:06 +0300

    commit v10

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0529/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task05.task0529;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Консоль-копилка
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        long l = 0;
+
+        System.out.println("Hello world No " + l + " !");
+    }
+}
+
